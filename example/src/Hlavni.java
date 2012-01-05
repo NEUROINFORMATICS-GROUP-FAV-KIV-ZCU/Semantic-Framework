c235593a0122de778c0ecf26bc70a02d261f547c
jakub_krauz <jakub_krauz@ce5181a5-0d73-4380-94cd-77e4cde4465f>
2012-01-05 10:39:56 +0000

    Release 2.4 created.
    git-svn-id: https://svn.origo.ethz.ch/jenabeanannotationextension/tags/release 2.4@127 ce5181a5-0d73-4380-94cd-77e4cde4465f

---
+++ example/src/Hlavni.java
@@ -0,0 +1,75 @@
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.util.List;
+
+import com.hp.hpl.jena.ontology.OntModelSpec;
+
+import tools.JenaBeanExtensionTool;
+import tools.OntologyProperties;
+import tools.OwlApiTool;
+import tools.Syntax;
+
+
+/**
+ * Třída, která plní funkci "uživatelského programu".
+ * Zde se vytvoří objekty určené k transformaci a zavolá se metoda, která proces spustí.
+ *
+ * @author Jakub Krauz
+ */
+public class Hlavni {
+	
+	private static InputStream is;
+
+    public static void main(String[] args) {
+    	
+    	/* vytvoreni objektu - musi provadet uzivatel knihovny sam */
+    	Example ex = new Example();
+    	List<Object> dataList = ex.getVstupniPole();
+    	
+    	JenaBeanExtensionTool jenaBean;
+    	//OwlApiTool owlApi;
+		try {
+			jenaBean = new JenaBeanExtensionTool(dataList);
+			jenaBean.setBasePackage("data.pojo");
+			jenaBean.setOntology(new OntologyProperties("http://www.kiv.zcu.cz/eegdatabase"));
+			is = jenaBean.getOntologyDocument(Syntax.RDF_XML_ABBREV);
+//			owlApi = new OwlApiTool(is);
+//			is = owlApi.convertToSemanticStandard("owl");
+			
+			/*jenaBean = new JenaBeanExtensionTool(dataList);
+			jenaBean.setBasePackage("data.pojo");
+			System.out.println("JenaBean: data nactena");
+			FileOutputStream out = new FileOutputStream(new File("ontologyDocument.owl"));
+			jenaBean.writeOntologyDocument(out, Syntax.RDF_XML_ABBREV);
+			out.close();
+			System.out.println("Zapsano do souboru.");*/
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+    	
+    	writeSemanticToFile("ontologyDocument.owl");
+
+    }
+    
+    
+    @SuppressWarnings("unused")
+	private static void writeSemanticToFile(String fileName) {
+    	System.out.println("- USER -");
+    	System.out.println("Creating file " + fileName + "...");
+    	try {
+			FileOutputStream out = new FileOutputStream(new File(fileName));
+			int pom;
+			while ((pom = is.read()) != -1) {
+				out.write(pom);
+			}
+			out.close();
+			System.out.println("File " + fileName + " was created.");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+    }
+    
+}
