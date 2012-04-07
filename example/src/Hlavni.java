956913168b2db030dedd5c5363fa1c1d9c15fe94
jakub_krauz <jakub_krauz@ce5181a5-0d73-4380-94cd-77e4cde4465f>
2012-04-07 15:20:23 +0000

    release 3.1 created.
    git-svn-id: https://svn.origo.ethz.ch/jenabeanannotationextension/tags/release 3.1@138 ce5181a5-0d73-4380-94cd-77e4cde4465f

---
+++ example/src/Hlavni.java
@@ -0,0 +1,102 @@
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.List;
+import java.util.Locale;
+
+import com.hp.hpl.jena.ontology.OntModelSpec;
+
+import tools.JenaBeanExtensionTool;
+import tools.Ontology;
+import tools.OwlApiTool;
+import tools.Syntax;
+
+
+/**
+ * Třída, která plní funkci "uživatelského programu".
+ * Zde se vytvoří objekty určené k transformaci a zavolá se metoda, která proces spustí.
+ *
+ * @author Jakub Krauz
+ */
+public class Hlavni {
+	
+	private static InputStream is;
+
+    public static void main(String[] args) {
+    	
+    	/* vytvoreni objektu - musi provadet uzivatel knihovny sam */
+    	//ExampleHodneDat ex = new ExampleHodneDat(1000);
+    	Example ex = new Example();
+    	List<Object> dataList = ex.getVstupniPole();
+    	
+    	JenaBeanExtensionTool jenaBean;
+    	OwlApiTool owlApi;
+		try {
+			jenaBean = new JenaBeanExtensionTool();
+			jenaBean.loadStatements(new FileInputStream("ontology.owl"));
+			jenaBean.loadOOM(dataList, false);
+//			Ontology ontology = new Ontology("http://kiv.zcu.cz/eegbase/2.1");
+//			//ontology.setVersionInfo(df.format(new Date()));
+//			ontology.setBackwardCompatibleWith("http://kiv.zcu.cz/eegbase/1.6");
+//			ontology.setPriorVersion("http://kiv.zcu.cz/eegdatabase");
+//			ontology.setIncompatibleWith("http://kiv.zcu.cz/ontology");
+//			ontology.setComment("EEG/ERP ontology v 2.06");
+//			ontology.setSeeAlso("http://eegdatabase.kiv.zcu.cz");
+//			//ontology.addImport("www.some.address.cz/importedOntology");
+//			jenaBean.setOntology(ontology);
+			
+			jenaBean.declareAllClassesDisjoint();
+			is = jenaBean.getOntologyDocument(Syntax.RDF_XML_ABBREV);
+			//is = jenaBean.getOntologySchema(Syntax.RDF_XML_ABBREV);
+//			owlApi = new OwlApiTool(is);
+//			is = owlApi.convertToSemanticStandard("rdf");
+			
+			/*jenaBean = new JenaBeanExtensionTool(dataList);
+			jenaBean.setBasePackage("data.pojo");
+			System.out.println("JenaBean: data nactena");
+			FileOutputStream out = new FileOutputStream(new File("ontologyDocument.owl"));
+			jenaBean.writeOntologyDocument(out, Syntax.RDF_XML_ABBREV);
+			out.close();
+			System.out.println("Zapsano do souboru.");*/
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+    	
+    	writeSemanticToFile("ontDocument.owl");
+
+    }
+    
+    
+    @SuppressWarnings("unused")
+	private static void writeSemanticToFile(String fileName) {
+    	System.out.println("- USER -");
+    	System.out.println("Creating file " + fileName + "...");
+    	try {
+			FileOutputStream out = new FileOutputStream(new File(fileName));
+			int pom;
+			while ((pom = is.read()) != -1) {
+				out.write(pom);
+			}
+			out.close();
+			System.out.println("File " + fileName + " was created.");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+    }
+    
+    
+    @SuppressWarnings("unused")
+	private static void vypisTrvani(long millis, String usek) {
+		long min = millis / 60000;
+		long sec = (millis - min * 60000) / 1000;
+		System.out.println(usek + ": " + min + " min, " + sec + " sec");
+    }
+    
+}
